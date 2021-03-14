d155a46a9769c3342c2927e0cc5af32743fb580b
soduma <soduma@naver.com>
2021-03-14 18:25:44 +0900

    중간점검

--- Meteor/MeteorViewController.swift
+++ Meteor/MeteorViewController.swift
@@ -16,6 +16,26 @@ class MeteorViewController: UIViewController {
 
 }
 
+extension MeteorViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MeteorCell", for: indexPath) as? MeteorCell else { return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+}
+
+
+
+class MeteorCell: UICollectionViewCell {
+    
+    @IBOutlet weak var someLabel: UILabel!
+}
+
 class MeteorHeaderView: UICollectionReusableView {
     
     @IBOutlet weak var sectionTitleLabel: UILabel!
