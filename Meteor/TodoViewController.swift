02c9550cef7dc704a634fb8c1994e1fd9daf3ad6
soduma <soduma@naver.com>
2021-03-16 16:47:29 +0900

    "

---
+++ Meteor/TodoViewController.swift
@@ -0,0 +1,63 @@
+//
+//  TodoViewController.swift
+//  Meteor
+//
+//  Created by 장기화 on 2021/03/14.
+//
+
+import UIKit
+
+class TodoViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension TodoViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TodoCell", for: indexPath) as? TodoCell else { return UICollectionViewCell()
+        }
+        return cell
+    }
+    
+    
+}
+
+extension TodoViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width: CGFloat = collectionView.bounds.width
+        let height: CGFloat = 50
+        return CGSize(width: width, height: height)
+    }
+}
+
+class TodoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+//        reset()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+//        reset()
+    }
+}
