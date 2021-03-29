8800823004138e6d5c93d80bfcf0021ca28bb991
soduma <soduma@naver.com>
2021-03-29 16:31:59 +0900

    meteor ui 수정

--- Meteor/ViewControllers/MeteorViewController.swift
+++ Meteor/ViewControllers/MeteorViewController.swift
@@ -9,17 +9,17 @@ import UIKit
 import UserNotifications
 
 class MeteorViewController: UIViewController {
-
+    
     @IBOutlet weak var meteorTextField: UITextField!
     @IBOutlet weak var meteorButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-                
+        
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge], completionHandler: {didAllow, Error in
                                                                     print(didAllow)})
-        }
+    }
     
     @IBAction func tapBG(_ sender: UITapGestureRecognizer) {
         meteorTextField.resignFirstResponder()
@@ -28,19 +28,19 @@ class MeteorViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         for index in 1...5 {
-        
-        let contents = UNMutableNotificationContent()
-        contents.title = "가나다라"
-        contents.subtitle = "마바사"
-        contents.body = "아자차카"
+            
+            let contents = UNMutableNotificationContent()
+            contents.title = "METEOR:"
+            contents.subtitle = "마바사"
+            contents.body = "아자차카"
             contents.badge = NSNumber(value: index)
-        
+            
             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 3, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: "\(index)timerdone", content: contents, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+            
+            let request = UNNotificationRequest(identifier: "\(index)timerdone", content: contents, trigger: trigger)
+            
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
         }
     }
     
-    }
+}
