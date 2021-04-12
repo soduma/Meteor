b905124e803b6316c3f4d8e121d242f1725eae0c
soduma <soduma@naver.com>
2021-04-12 18:46:48 +0900

    ui 수정

--- Meteor/ViewControllers/Todo/ModifyViewController.swift
+++ Meteor/ViewControllers/Todo/ModifyViewController.swift
@@ -11,7 +11,7 @@ class ModifyViewController: UIViewController {
     
     @IBOutlet weak var modifyTextView: UITextView!
     @IBOutlet weak var finishButton: UIButton!
-    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     
     let modifyViewModel = ModifyViewModel()
     let todoViewModel = TodoViewModel()
