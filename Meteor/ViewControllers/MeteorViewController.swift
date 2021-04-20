b245dee55f640255f04339f9a0e7b730f060eb58
soduma <soduma@naver.com>
2021-04-20 12:35:54 +0900

    알림 갯수 5개로 변경

--- Meteor/ViewControllers/MeteorViewController.swift
+++ Meteor/ViewControllers/MeteorViewController.swift
@@ -28,7 +28,7 @@ class MeteorViewController: UIViewController, GADFullScreenContentDelegate {
     var notificationIndex = 0
     var noticeIndex = 0
     var notice = ["내용을 작성하고 보내기를 누르면 알림으로 받을 수 있어요.",
-                  "알림은 3개까지 쌓이고, 먼저 온 알림부터 순차적으로 삭제됩니다.",
+                  "알림은 5개까지 쌓이고, 먼저 온 알림부터 순차적으로 삭제됩니다.",
                   "알림 창에 표시할 수 있는 텍스트의 길이에는 한계가 있습니다!"]
     
     // 구글광고!!!!!!!!!!!!!!!!!!!!!!
@@ -204,7 +204,7 @@ class MeteorViewController: UIViewController, GADFullScreenContentDelegate {
         }
         
         notificationIndex += 1
-        if notificationIndex > 2 {
+        if notificationIndex > 4 {
             notificationIndex = 0
         }
         
