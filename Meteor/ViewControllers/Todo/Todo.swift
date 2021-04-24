8c3265ac90f89a1497a302428bad8d640fc5647d
soduma <soduma@naver.com>
2021-04-24 15:47:16 +0900

    현지화 초기화

--- Meteor/ViewControllers/Todo/Todo.swift
+++ Meteor/ViewControllers/Todo/Todo.swift
@@ -37,7 +37,8 @@ class TodoManager {
     }
     
     func addTodo(_ todo: Todo) {
-        todos.append(todo)
+//        todos.append(todo)
+        todos.insert(todo, at: 0)
         saveTodo()
     }
     
