f750ef81bd27ed995b74e80678a03038d298e6cf
soduma <soduma@naver.com>
2021-05-08 20:02:32 +0900

    w

--- Meteor/TabBar.swift
+++ Meteor/TabBar.swift
@@ -13,9 +13,10 @@ class CustomTabBar: UITabBar {
         super.awakeFromNib()
         
         layer.masksToBounds = true
-        layer.cornerRadius = 25
-        layer.borderColor = UIColor.systemGray.cgColor
-        layer.borderWidth = 0.5
+        layer.cornerRadius = 20
+//        layer.borderColor = UIColor.systemGray.cgColor
+//        layer.borderWidth = 0.3
         layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
+        
     }
 }
