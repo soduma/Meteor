1e288690a23de3bbf421f5792c61e61d0e2de513
soduma <soduma@naver.com>
2021-07-26 14:11:02 +0900

    타이머전 저장

--- Meteor/SceneDelegate.swift
+++ Meteor/SceneDelegate.swift
@@ -24,16 +24,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-//        print("out")
-//        UserDefaults.standard.removeObject(forKey: "foreDate")
-//        UserDefaults.standard.removeObject(forKey: "backDate")
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-        
-//        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
@@ -44,16 +39,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-//        let foreDate = Date().timeIntervalSince1970
-//        UserDefaults.standard.setValue(foreDate, forKey: "foreDate")
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-//        let backDate = Date().timeIntervalSince1970
-//        UserDefaults.standard.setValue(backDate, forKey: "backDate")
     }
     
 }
