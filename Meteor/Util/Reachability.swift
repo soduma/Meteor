c04d82b383cde4a083ae4f479d8bf9713dc5aa4e
JANG KIHWA <soduma@naver.com>
2023-08-03 15:34:46 +0900

    update pod and modify files path

--- Meteor/Util/Reachability.swift
+++ Meteor/Util/Reachability.swift
@@ -10,9 +10,7 @@ import SystemConfiguration
 
 // 인터넷 연결확인
 public class Reachability {
-    
     class func isConnectedToNetwork() -> Bool {
-        
         var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
         zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
