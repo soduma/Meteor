6216f51feb55485b6e6504f41f2b075bece81142
JANG KIHWA <soduma@naver.com>
2023-09-03 21:01:02 +0900

    add shell for cloud

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Meteor
+#
+#  Created by 장기화 on 2023/09/03.
+#  
+
+# Update brew and install SwiftLint and Danger-swift
+brew update
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+
+# Install dependencies you manage with CocoaPods.
+pod install
