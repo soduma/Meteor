4b35a863ebae825372ee8a9868dce28ee1491bcb
JANG KIHWA <soduma@naver.com>
2023-09-06 08:51:20 +0900

    set minimum log

--- Meteor/Setting/AppDelegate.swift
+++ Meteor/Setting/AppDelegate.swift
@@ -12,9 +12,9 @@ import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         Thread.sleep(forTimeInterval: 0.5)
 //        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
         FirebaseApp.configure()
         return true
     }
