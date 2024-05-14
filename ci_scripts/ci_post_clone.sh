51cd48bd1d91b0f0db9d84077426c42008a7baa4
JANG KIHWA <soduma@naver.com>
2024-05-15 04:20:45 +0900

    ci update

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,7 +6,7 @@
 #  Created by 장기화 on 2023/09/03.
 #
 
-# 1. GOOGLE_SERVICE_INFO_PLIST 환경변수가 존재하는지 확인
+# 1. IDLIST_PLIST 환경변수가 존재하는지 확인
 if [ -n "$IDLIST_PLIST" ]; then
     echo "IDLIST_PLIST 환경변수가 발견되었습니다."
     
@@ -17,6 +17,17 @@ else
     echo "IDList.plist 환경변수가 존재하지 않습니다. 스크립트를 종료합니다."
 fi
 
+# 2. AUTHKEY_P8 환경변수가 존재하는지 확인
+if [ -n "$AUTHKEY_P8" ]; then
+    echo "AUTHKEY_P8 환경변수가 발견되었습니다."
+    
+    # 환경변수 값을 AuthKey.p8 파일에 저장
+    echo "$AUTHKEY_P8" > "$Meteor"/AuthKey.p8
+    echo "AuthKey.p8 파일을 프로젝트 디렉토리 내에 생성하였습니다."
+else
+    echo "AuthKey.p8 환경변수가 존재하지 않습니다. 스크립트를 종료합니다."
+fi
+
 exit 0
 
 # Update brew and install SwiftLint and Danger-swift
@@ -25,4 +36,4 @@ exit 0
 brew install cocoapods
 
 # Install dependencies you manage with CocoaPods.
-pod update
+pod install
