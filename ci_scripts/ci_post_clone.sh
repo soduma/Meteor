bcd045cc8ae60c32fb652eb89c6747c1834f37d2
JANG KIHWA <soduma@naver.com>
2024-05-16 15:19:37 +0900

    update.

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -13,9 +13,9 @@ echo "Stage: ci_post_clone is activated .... "
 
 cd ../Meteor/
 
-plutil -replace AUTHKEY_P8 -string $AUTHKEY_P8 "$CI_WORKSPACE"/"$Meteor"/Info.plist
-plutil -replace TEAM_ID -string $TEAM_ID "$CI_WORKSPACE"/"$Meteor"/Info.plist
-plutil -replace KEY_ID -string $KEY_ID "$CI_WORKSPACE"/"$Meteor"/Info.plist
+plutil -replace AUTHKEY_P8 -string $AUTHKEY_P8 $CI_WORKSPACE/$Meteor/Info.plist
+plutil -replace TEAM_ID -string $TEAM_ID $CI_WORKSPACE/$Meteor/Info.plist
+plutil -replace KEY_ID -string $KEY_ID $CI_WORKSPACE/$Meteor/Info.plist
 
 plutil -p "$CI_WORKSPACE"/"$Meteor"/Info.plist
 
