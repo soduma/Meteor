730cfd21808965c28989a34f5abbc9163a3f645f
JANG KIHWA <soduma@naver.com>
2024-05-16 11:43:12 +0900

    update for parser

--- Meteor/Common/JWTParse/FileParser.swift
+++ Meteor/Common/JWTParse/FileParser.swift
@@ -9,25 +9,36 @@ import Foundation
 
 struct FileParser {
     static var teamID: String {
+        #if DEBUG
         guard let file = Bundle.main.path(forResource: "IDList", ofType: "plist") else { return "" }
         guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
         guard let key = resource["TEAM_ID"] as? String else {
             fatalError()
         }
         return key
+        #elseif RELEASE
+        guard let apiKey = Bundle.main.infoDictionary?["TEAM_ID"] as? String else { return "" }
+        return apiKey
+        #endif
     }
     
     static var keyID: String {
+        #if DEBUG
         guard let file = Bundle.main.path(forResource: "IDList", ofType: "plist") else { return "" }
         guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
         guard let key = resource["KEY_ID"] as? String else {
             fatalError()
         }
         return key
+        #elseif RELEASE
+        guard let apiKey = Bundle.main.infoDictionary?["KEY_ID"] as? String else { return "" }
+        return apiKey
+        #endif
     }
     
     /// Convert PEM format .p8 file to DER-encoded ASN.1 data
     static func parse() -> P8Payload? {
+        #if DEBUG
         guard let path = Bundle.main.path(forResource: "AuthKey", ofType: "p8") else { return nil }
         let pathURL = URL(filePath: path)
         guard let content = try? String(contentsOf: pathURL) else { return nil }
@@ -36,5 +47,9 @@ struct FileParser {
             .joined(separator: "")
         
         return P8Payload(keyContent)
+        #elseif RELEASE
+        guard let apiKey = Bundle.main.infoDictionary?["AUTHKEY_P8"] as? String else { return nil }
+        return P8Payload(apiKey)
+        #endif
     }
 }
--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,27 +6,20 @@
 #  Created by 장기화 on 2023/09/03.
 #
 
-# 1. IDLIST_PLIST 환경변수가 존재하는지 확인
-if [ -n "$IDLIST_PLIST" ]; then
-    echo "IDLIST_PLIST 환경변수가 발견되었습니다."
-    
-    # 환경변수 값을 IDList.plist 파일에 저장
-    echo "$IDLIST_PLIST" > "$CI_PROJECT_FILE_PATH"/"$Meteor"/IDList.plist
-    echo "IDList.plist 파일을 프로젝트 디렉토리 내에 생성하였습니다."
-else
-    echo "IDList.plist 환경변수가 존재하지 않습니다. 스크립트를 종료합니다."
-fi
+echo "Stage: ci_post_clone is activated .... "
 
-# 2. AUTHKEY_P8 환경변수가 존재하는지 확인
-if [ -n "$AUTHKEY_P8" ]; then
-    echo "AUTHKEY_P8 환경변수가 발견되었습니다."
-    
-    # 환경변수 값을 AuthKey.p8 파일에 저장
-    echo "$AUTHKEY_P8" > "$CI_PROJECT_FILE_PATH"/"$Meteor"/AuthKey.p8
-    echo "AuthKey.p8 파일을 프로젝트 디렉토리 내에 생성하였습니다."
-else
-    echo "AuthKey.p8 환경변수가 존재하지 않습니다. 스크립트를 종료합니다."
-fi
+# for future reference
+# https://developer.apple.com/documentation/xcode/environment-variable-reference
+
+cd ../Meteor/
+
+plutil -replace AUTHKEY_P8 -string $AUTHKEY_P8 Info.plist
+plutil -replace TEAM_ID -string $TEAM_ID Info.plist
+plutil -replace KEY_ID -string $KEY_ID Info.plist
+
+plutil -p Info.plist
+
+echo "Stage: ci_post_clone is DONE .... "
 
 # Update brew and install SwiftLint and Danger-swift
 #brew update
